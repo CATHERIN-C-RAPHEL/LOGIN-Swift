87cf035e25e5728d863d655934308d3048a0a335
CATHERIN-C-RAPHEL <catherincr19@gmail.com>
2023-08-02 14:30:01 +0530

    first commit

--- catherin-LOGIN/ViewController.swift
+++ catherin-LOGIN/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textemail: UITextField!
+    @IBOutlet weak var textpass: UITextField!
+    
+    @IBOutlet weak var plaintext: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func valid(_ sender: Any) {
+        let email = textemail.text
+        let pass = textpass.text
+        if(email == "ddukk" && pass == "1234"){
+            plaintext.text = "correct"
+        }else{
+            plaintext.text = "incorrect"
+        }
+    }
+    
+    
 }
 
